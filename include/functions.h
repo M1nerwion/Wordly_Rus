02fb527e9f0400509941697f27bfea93b113a6cc
Nikpop228 <nikipet15.06@gmail.com>
2025-07-06 00:11:49 +0300

    fix: широкие версии потоков и строк, не работает обработка для коротких и длинных слов

--- include/functions.h
+++ include/functions.h
@@ -4,20 +4,21 @@
 #include		<vector>
 #include		<string>
 #include		<random>
-#define ERASE   "\x1b[1F\x1b[2K"   				// стереть строку после cin
-#define BY      "\x1b[2;30;43m"   				// вывод черный текст на желтом фоне
-#define BG      "\x1b[2;30;42m"  				// вывод черный текст на зеленом фоне
-#define BR      "\x1b[2;30;41m"                 // вывод черный текст на красном фоне
-#define BGRAY   "\x1b[38;5;0m\x1b[48;5;245m"    // черный текст на сером фоне
-#define RESET   "\x1b[0m"						// сбросить цвет
+#include        <locale>
+#define ERASE   "\x1b[1F\x1b[2K"   				// пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ cin
+#define BY      "\x1b[2;30;43m"   				// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
+#define BG      "\x1b[2;30;42m"  				// пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
+#define BR      "\x1b[2;30;41m"                 // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
+#define BGRAY   "\x1b[38;5;0m\x1b[48;5;245m"    // пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
+#define RESET   "\x1b[0m"						// пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ
 
-//extern int n; // количество букв в слове
-//extern int count_of_attemp; // количество попыток
+//extern int n; // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅ
+//extern int count_of_attemp; // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ
 
-void read(std::vector<char>& vec);
-void color_processing(std::vector<char>& vec_user, std::vector<char>& vec_hidden_word);
+void read(std::vector<wchar_t>& vec);
+void color_processing(std::vector<wchar_t>& vec_user, std::vector<wchar_t>& vec_hidden_word);
 long long amount_of_words(std::string file_slov);
-std::vector<char> transform(std::string line);
-std::vector<char> choice_of_random_word(std::string file_slov);
-bool check_letters(std::vector<char> line, std::vector<char> user_word);
-bool check_on_word(std::vector<char> user_word, std::string file_slov);
+std::vector<wchar_t> transform(std::wstring line);
+std::vector<wchar_t> choice_of_random_word(std::string file_slov);
+bool check_letters(std::vector<wchar_t> line, std::vector<wchar_t> user_word);
+bool check_on_word(std::vector<wchar_t> user_word, std::string file_slov);
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,18 @@ extern int count_of_attemp = 0; // количество попыток
 
 int main(){
 	srand(time(0));
-	setlocale(LC_ALL, "ru_RU.UTF-8");
+	std::locale::global(std::locale("ru_RU.UTF-8"));
+	//setlocale(LC_ALL, "ru_RU.UTF-8");
 	std::ios_base::sync_with_stdio(false); // отключение синхронизации потоков, чтобы на линуксе не ломался вывод
 	
 	//std::string file_slov = "fiveletters.txt";
 	std::string file_slov = "src/g.txt";
-	std::vector<char> answer_word = choice_of_random_word(file_slov);
-	std::cout << "   ";
-	for (auto p : answer_word) { std::cout << p; }
-	std::cout << '\n';
+	std::vector<wchar_t> answer_word = choice_of_random_word(file_slov);
+	std::wcout << "   ";
+	for (auto p : answer_word) { std::wcout << p; }
+	std::wcout << '\n';
 
-	std::vector<char> user_word(n+1); // вектор всегда будет больше 
+	std::vector<wchar_t> user_word(n+1); // вектор всегда будет больше 
 	while (count_of_attemp < 6) {
 		read(user_word);
 		if (check_letters(answer_word, user_word)) {
@@ -28,14 +29,14 @@ int main(){
 			continue;
 		}
 		else {
-			std::cout << BR << "Ошибка, такого слова не существует" << RESET << "\nНажмите ENTER, чтобы повторить попытку ..." << std::endl;
-			std::cin.ignore(std::cin.rdbuf()->in_avail());
-            std::cin.get(); // ожидание нажатия
-            std::cout << "\x1b[4F\x1b[0J"; // очистка консоли от длинного слова и предкпреждения
-			std::cin.ignore(std::cin.rdbuf()->in_avail());
+			std::wcout << BR << "Ошибка, такого слова не существует" << RESET << "\nНажмите ENTER, чтобы повторить попытку ..." << std::endl;
+			std::wcin.ignore(std::wcin.rdbuf()->in_avail());
+            std::wcin.get(); // ожидание нажатия
+            std::wcout << "\x1b[4F\x1b[0J"; // очистка консоли от длинного слова и предкпреждения
+			std::wcin.ignore(std::wcin.rdbuf()->in_avail());
 		}
 	}
 	if (count_of_attemp == 6) {
-		std::cout << "ДОЛБОЕБ\n";
+		std::wcout << "ДОЛБОЕБ\n";
 	}
 }
